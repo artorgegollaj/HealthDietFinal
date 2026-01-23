dd187ed79b98edd603db25113731ce66a6626d52
artorgegollaj <ag73085@ubt-uni.net>
2026-01-23 18:43:21 +0100

    PHP conversion

--- Projekti.php
+++ Projekti.php
@@ -1,4 +1,5 @@
-Projekti.php
+<?php
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
---
+++ faqja4.php
@@ -0,0 +1,62 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Faqja4</title>
+    <link rel="stylesheet" href="css4.css">
+</head>
+<body>
+    <header class="header">
+        <div class="logo"><img src="Logo1.png" alt="logo"></div>
+        <nav class="nav">
+            <a href="Projektifq2.html">Workouts</a>
+            <a href="faqja3.html">Programs</a>
+            <a href="#">Healthy Living</a>
+            <a href="#">About</a>
+            <a href="#">Membership</a>
+        </nav>
+        </header>
+    <div class="Signup">
+        
+        <h1>Sign Up</h1>
+        <p class="info">Please give your information to continue!</p>
+        <form id="signup">
+            <input type="text" id="name" placeholder="First Name" >
+            <p id="nameError" style="color:red; font-size:14px;"></p>
+            <input type="text" id="lastname" placeholder="Last Name" >
+            <p id="lastNameError" style="color:red; font-size:14px;"></p>
+            <input type="text" id="height" placeholder="Height" >
+            <p id="heightError" style="color:red; font-size:14px;"></p>
+            <input type="text" id="weight" placeholder="Weight" >
+            <p id="weightError" style="color:red; font-size:14px;"></p>
+            <input type="email" id="email" placeholder="Email">
+            <p id="emailError" style="color:red; font-size:14px;"></p>
+            <input type="password" id="password" placeholder="Password">
+            <p id="passError" style="color:red; font-size:14px;"></p>
+            <input type="password" id="confirmpassword" placeholder="Confirm Password">
+            <p id="cpassError" style="color:red; font-size:14px;"></p>
+            <div class="remember">
+                    <input type="checkbox" id="rememberMe">
+                    <label for="rememberMe">Remember Me</label>
+            </div>
+            <button type="submit" class="signupb">Sign Up</button>
+        </form>
+    </div>
+    <footer class="footer">
+        <div class="content">
+            <p>© 2025 Health And Diet. All rights reserved.</p>
+            <div class="links">
+                <a href="#">Privacy Policy</a>
+                <a href="#">Terms of Service</a>
+                <a href="#">Contact Us</a>
+            </div>
+        </div>
+    </footer>
+    <script src="script4.js"></script>
+</body>
+</html>
+</body>
+</html>
